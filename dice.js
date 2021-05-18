c03ff8742b2fa6ddc7a8ee47b6da1ae738efeaa0
Vaibhav <vvaibhav@apple.com>
2021-05-18 15:49:53 -0700

    adding exploration graphs

--- dice.js
+++ dice.js
@@ -13,6 +13,7 @@ var currMove = null
 var autoOrientation = false
 var isHighlight = true
 var isActive = false
+var graph = false
 
 function resetBoard () {
   var config = {
@@ -207,6 +208,13 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+var treeData = [
+  {
+    "name": "Depth 0",
+    "parent": "null"
+  }
+];
+
 function shuffle(arr) {
   return arr
           .map((a) => ({sort: Math.random(), value: a}))
@@ -225,6 +233,16 @@ async function makeRandomMoves (depth, ms) {
       board.position(game.fen());
       $fen.html(game.fen())
       $pgn.html(game.pgn())
+      if (graph) {
+        if (!treeData[0].hasOwnProperty("children")) {
+          treeData[0]["children"] = [];
+        }
+        treeData[0].children.push({
+          "name": "1: " + possibleMoves[i],
+          "parent": "Depth 0"
+        });
+        update(treeData[0]);
+      }
       await sleep(ms);
 
       if (depth > 1) {
@@ -239,6 +257,16 @@ async function makeRandomMoves (depth, ms) {
             board.position(game.fen());
             $fen.html(game.fen())
             $pgn.html(game.pgn())
+            if (graph) {
+              if (!treeData[0].children[i].hasOwnProperty("children")) {
+                treeData[0].children[i]["children"] = [];
+              }
+              treeData[0].children[i].children.push({
+                "name": "2: " + possibleBlackMoves[j],
+                "parent": "1"
+              });
+              update(treeData[0]);
+            }
             await sleep(ms);
 
             if (depth > 2) {
@@ -253,6 +281,16 @@ async function makeRandomMoves (depth, ms) {
                   board.position(game.fen());
                   $fen.html(game.fen())
                   $pgn.html(game.pgn())
+                  if (graph) {
+                    if (!treeData[0].children[i].children[j].hasOwnProperty("children")) {
+                      treeData[0].children[i].children[j]["children"] = [];
+                    }
+                    treeData[0].children[i].children[j].children.push({
+                      "name": "3: " + possibleMoves1[k],
+                      "parent": "2"
+                    });
+                    update(treeData[0]);
+                  }
                   await sleep(ms);
 
                   if (depth > 3) {
@@ -267,6 +305,16 @@ async function makeRandomMoves (depth, ms) {
                         board.position(game.fen());
                         $fen.html(game.fen())
                         $pgn.html(game.pgn())
+                        if (graph) {
+                          if (!treeData[0].children[i].children[j].children[k].hasOwnProperty("children")) {
+                            treeData[0].children[i].children[j].children[k]["children"] = [];
+                          }
+                          treeData[0].children[i].children[j].children[k].children.push({
+                            "name": "4: " + possibleMoves2[l],
+                            "parent": "3"
+                          });
+                          update(treeData[0]);
+                        }
                         await sleep(ms);
 
                         if (depth > 4) {
@@ -281,6 +329,16 @@ async function makeRandomMoves (depth, ms) {
                               board.position(game.fen());
                               $fen.html(game.fen())
                               $pgn.html(game.pgn())
+                              if (graph) {
+                                if (!treeData[0].children[i].children[j].children[k].children[l].hasOwnProperty("children")) {
+                                  treeData[0].children[i].children[j].children[k].children[l]["children"] = [];
+                                }
+                                treeData[0].children[i].children[j].children[k].children[l].children.push({
+                                  "name": "5: " + possibleMoves3[m],
+                                  "parent": "4"
+                                });
+                                update(treeData[0]);
+                              }
                               await sleep(ms);
 
                               if (depth > 5) {
@@ -295,6 +353,16 @@ async function makeRandomMoves (depth, ms) {
                                     board.position(game.fen());
                                     $fen.html(game.fen())
                                     $pgn.html(game.pgn())
+                                    if (graph) {
+                                      if (!treeData[0].children[i].children[j].children[k].children[l].children[m].hasOwnProperty("children")) {
+                                        treeData[0].children[i].children[j].children[k].children[l].children[m]["children"] = [];
+                                      }
+                                      treeData[0].children[i].children[j].children[k].children[l].children[m].children.push({
+                                        "name": "6: " + possibleMoves4[n],
+                                        "parent": "5"
+                                      });
+                                      update(treeData[0]);
+                                    }
                                     await sleep(ms);
 
                                     if (depth > 6) {
@@ -424,4 +492,146 @@ function startMakingMoves() {
   }
 }
 
+function turnOnGraph() {
+  var x = document.getElementById("graph");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    graph = true
+    document.getElementById('flipOrientationBtn').innerHTML = "Turn Off Exploration Graph"
+  } else {
+    graph = false
+    x.style.display = "none";
+    document.getElementById('flipOrientationBtn').innerHTML = "Turn On Exploration Graph"
+  }
+}
+
+// ************** Generate the tree diagram	 *****************
+var margin = {top: 0, right: 10, bottom: 20, left: 20},
+  width = 960 - margin.right - margin.left,
+  height = 500 - margin.top - margin.bottom;
+
+var i = 0,
+  duration = 750,
+  root;
+
+var tree = d3.layout.tree()
+  .size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+  .projection(function(d) { return [d.y, d.x]; });
+
+var svg = d3.select("#graph").append("svg")
+  .attr("width", width + margin.right + margin.left)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+root = treeData[0];
+root.x0 = height / 2;
+root.y0 = 0;
+
+update(root);
+
+d3.select(self.frameElement).style("height", "500px");
+
+function update(source) {
+
+  // Compute the new tree layout.
+  var nodes = tree.nodes(root).reverse(),
+    links = tree.links(nodes);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+  // Update the nodes…
+  var node = svg.selectAll("g.node")
+    .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+  // Enter any new nodes at the parent's previous position.
+  var nodeEnter = node.enter().append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+    .on("click", click);
+
+  nodeEnter.append("circle")
+    .attr("r", 1e-6)
+    .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeEnter.append("text")
+    .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
+    .attr("dy", ".35em")
+    .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+    .text(function(d) { return d.name; })
+    .style("fill-opacity", 1e-6);
+
+  // Transition nodes to their new position.
+  var nodeUpdate = node.transition()
+    .duration(duration)
+    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+  nodeUpdate.select("circle")
+    .attr("r", 10)
+    .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeUpdate.select("text")
+    .style("fill-opacity", 1);
+
+  // Transition exiting nodes to the parent's new position.
+  var nodeExit = node.exit().transition()
+    .duration(duration)
+    .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+    .remove();
+
+  nodeExit.select("circle")
+    .attr("r", 1e-6);
+
+  nodeExit.select("text")
+    .style("fill-opacity", 1e-6);
+
+  // Update the links…
+  var link = svg.selectAll("path.link")
+    .data(links, function(d) { return d.target.id; });
+
+  // Enter any new links at the parent's previous position.
+  link.enter().insert("path", "g")
+    .attr("class", "link")
+    .attr("d", function(d) {
+    var o = {x: source.x0, y: source.y0};
+    return diagonal({source: o, target: o});
+    });
+
+  // Transition links to their new position.
+  link.transition()
+    .duration(duration)
+    .attr("d", diagonal);
+
+  // Transition exiting nodes to the parent's new position.
+  link.exit().transition()
+    .duration(duration)
+    .attr("d", function(d) {
+    var o = {x: source.x, y: source.y};
+    return diagonal({source: o, target: o});
+    })
+    .remove();
+
+  // Stash the old positions for transition.
+  nodes.forEach(function(d) {
+  d.x0 = d.x;
+  d.y0 = d.y;
+  });
+}
+
+// Toggle children on click.
+function click(d) {
+  if (d.children) {
+  d._children = d.children;
+  d.children = null;
+  } else {
+  d.children = d._children;
+  d._children = null;
+  }
+  update(d);
+}
+
 $('#startPositionBtn').on('click', startMakingMoves)
+$('#flipOrientationBtn').on('click', turnOnGraph)
